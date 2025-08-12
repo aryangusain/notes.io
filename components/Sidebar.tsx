2fb7e3ad70f6e3eb11ed5268ab4896eded67ed11
aryangusain <aryangusain123@gmail.com>
2025-08-12 15:06:02 +0530

    improve sidebar toggle

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -51,8 +51,8 @@ const Sidebar = () => {
       className={cn(
         "rounded-r-xl shadow-xl flex flex-col gap-[40px] h-full dark:bg-[#27272a] bg-neutral-200 z-20 absolute top-0 left-0 transition-all duration-200 ease-in-out py-[60px] overflow-hidden",
         sidebarOpen
-          ? "w-[280px] px-[20px] opacity-100"
-          : " w-0 px-[0px] opacity-0"
+          ? "w-[280px] px-[20px]"
+          : " w-0 px-[0px]"
       )}
     >
       <IconX
